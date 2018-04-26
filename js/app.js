353af27d950da0cd4dfeda7a55033a1befdeaacd
Sergiu <sil3ent@yahoo.com>
2018-04-26 18:38:13 +0300

    Add player class and methods

--- js/app.js
+++ js/app.js
@@ -31,7 +31,24 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 380;
+}
 
+Player.prototype.update = function () {
+    this.x = 200;
+    this.y = 380;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function () {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
