1524449cff93dea0df47e017f90fc4da42e97a3c
Sergiu <sil3ent@yahoo.com>
2018-04-28 20:59:02 +0300

    Feat: add random number generator function

--- js/app.js
+++ js/app.js
@@ -66,6 +66,10 @@ allEnemies = [];
 // Place the player object in a variable called player
 var player = new Player();
 
+// Function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
